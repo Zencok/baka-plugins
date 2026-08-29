9c7b936be01d04380fba9e7d2e172c32732f4ede
zencok <266256496+Zencok@users.noreply.github.com>
2026-08-30 01:22:31 +0800

    fix(qq): use musicu playlist import

--- plugins/qq.js
+++ plugins/qq.js
@@ -1004,35 +1004,70 @@ async function importMusicSheet(urlLike) {
     return;
   }
 
+  // 使用 musicu 常规歌单接口，loginUin 固定为 0。
   const result = (
     await axios_1.default({
-      url: `http://i.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&utf8=1&disstid=${id}&loginUin=0`,
-      headers: { Referer: "https://y.qq.com/n/yqq/playlist", Cookie: "uin=" },
-      method: "get",
-      xsrfCookieName: "XSRF-TOKEN",
-      withCredentials: true,
+      url: "https://u.y.qq.com/cgi-bin/musicu.fcg",
+      method: "POST",
+      data: {
+        loginUin: 0,
+        comm: {
+          ct: 24,
+          cv: 4747474,
+          uin: 0,
+        },
+        req: {
+          module: "music.srfDissInfo.aiDissInfo",
+          method: "uniform_get_Dissinfo",
+          param: {
+            disstid: Number(id),
+            userinfo: 1,
+            tag: 1,
+            orderlist: 1,
+            song_begin: 0,
+            song_num: 1000,
+            onlysonglist: 0,
+            enc_host_uin: "",
+          },
+        },
+      },
+      headers: {
+        Referer: "https://y.qq.com/",
+        Origin: "https://y.qq.com",
+        "User-Agent": headers["user-agent"],
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      timeout: 20000,
     })
   ).data;
-  const res = JSON.parse(
-    result.replace(/callback\(|MusicJsonCallback\(|jsonCallback\(|\)$/g, "")
-  );
 
-  const sheetData = res.cdlist?.[0];
-  if (!sheetData) {
+  const playlistData = result?.req?.data;
+  if (
+    result?.code !== 0 ||
+    result?.req?.code !== 0 ||
+    playlistData?.code !== 0 ||
+    !playlistData?.dirinfo
+  ) {
     return;
   }
-  const songList = sheetData.songlist || [];
+  const sheetData = playlistData.dirinfo;
+  const songList = playlistData.songlist || [];
   const qualityInfo = await getBatchQualities(songList);
-  const musicList = songList.map(song => formatMusicItem(song, qualityInfo));
+  const musicList = songList.map((song) => formatMusicItem(song, qualityInfo));
 
   return {
-    id: String(sheetData.disstid || id),
-    title: he.decode(sheetData.dissname || sheetData.dirname || ""),
-    artwork: sheetData.logo || sheetData.logo1,
-    artist: sheetData.nickname || sheetData.nick || "",
+    id: String(sheetData.disstid || sheetData.id || id),
+    title: he.decode(sheetData.dissname || sheetData.dirname || sheetData.title || ""),
+    artwork: sheetData.logo || sheetData.logo1 || sheetData.picurl || sheetData.picurl2,
+    artist:
+      sheetData.nickname ||
+      sheetData.nick ||
+      sheetData.host_nick ||
+      sheetData.creator?.nick ||
+      "",
     description: he.decode(sheetData.desc || sheetData.desc2 || ""),
-    worksNum: Number(sheetData.songnum) || musicList.length,
-    playCount: Number(sheetData.visitnum) || 0,
+    worksNum: Number(sheetData.songnum || sheetData.song_num) || musicList.length,
+    playCount: Number(sheetData.visitnum || sheetData.listennum) || 0,
     createAt: Number(sheetData.ctime) > 0 ? Number(sheetData.ctime) * 1000 : undefined,
     musicList,
   };
@@ -1267,7 +1302,7 @@ function getMusicDetailPageUrl(musicItem) {
 module.exports = {
   platform: "QQ音乐",
   author: "Toskysun",
-  version: "1.1.0",
+  version: "1.1.1",
   srcUrl: UPDATE_URL,
   cacheControl: "no-cache",
   primaryKey: ["id", "songmid"],
